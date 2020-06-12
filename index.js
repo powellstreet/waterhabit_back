e504e8d1ce1dc886ee825d4d3a9d536b87697280
powellstreet <codestates.jc@gmail.com>
2020-06-12 17:51:24 +0900

    :zap: Route 설정 - user.js, like.js 추가 - index.js에 route 설정, 모듈 설정

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const user = require('./routes/user');
+const like = require('./routes/like');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan());
+
+app.use('/user', user);
+app.use('like', like)
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
---
+++ routes/like.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+// route 세부설정하기
+
+module.exports = router;
