c1826299e29798fa2ff1ea5def0464093251211d
powellstreet <codestates.jc@gmail.com>
2020-06-24 23:57:21 +0900

    :zap: 기록 가져오기 API 추가
    - getStamp API 추가, route 추가

--- controllers/records.js
+++ controllers/records.js
@@ -1,8 +1,15 @@
 const { Records } = require('../models');
 
 module.exports = {
+  getStamp: (req, res) => {
+    const { userId } = req.body;
+    Records.findAll({ where: { userId } })
+      .then((instance) => res.status(200).json(instance))
+      .catch((err) => res.json(err));
+  },
+
   updateStamp: (req, res) => {
-    const day = 1;
+    const { day } = req.body;
     Records.findOrCreate({ where: { userId: 1, day } })
       .then((instance) => res.status(200).json(instance))
       .catch((err) => res.json(err));
--- routes/records.js
+++ routes/records.js
@@ -3,6 +3,7 @@ const controllers = require('../controllers/records');
 
 const router = express.Router();
 
+router.post('/getStamp', controllers.getStamp);
 router.post('/updateStamp', controllers.updateStamp);
 
 module.exports = router;
